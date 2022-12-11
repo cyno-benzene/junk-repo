e21430884e7b95cbc1d20d989fd1538e5d070c58
your_moms_friend <abhayrane1@gmail.com>
2022-12-11 17:43:41 +0530

    Image flushing added

--- app.py
+++ app.py
@@ -1,13 +1,20 @@
 from flask import Flask, render_template,request
-from script import resolute
+from script import resolute, shaper
 import os
+import base64, io
+from PIL import Image
 
 
 app = Flask(__name__)
 IMG_FOLDER = os.path.join('static','IMG')
+global token
+token = False
+
 @app.route('/')
 def home():
+    cacheWipe()
     return render_template('index.html')
+
 
 @app.route('/', methods=['GET', 'POST'])
 def output():
@@ -15,8 +22,25 @@ def output():
         inp = request.form.to_dict()
         numb = inp['phNo']
         resolute(0,0,numb)
-    return render_template('index.html')
+        encoded_img_data=showOnWeb(token=True)
+        cacheWipe()
+    return render_template('index.html', img_data=encoded_img_data.decode('utf-8'))
 
+
+def showOnWeb(token):
+    if(token):
+        im=Image.open("foo.png")
+        data = io.BytesIO()
+        im.save(data, "PNG")
+        encoded_img_data = base64.b64encode(data.getvalue())
+        return encoded_img_data
+
+
+def cacheWipe():
+    if os.path.exists("foo.png"):
+        os.remove('foo.png')
+    else:
+        print('cache clean')
 
 
 if __name__ == '__main__':
